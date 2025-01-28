ca5355c858cb7b723fc34d56af582ef7b234cba3
FABvivek <vivekvs5475543@gmail.com>
2025-01-28 13:39:31 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -38,7 +38,7 @@ products.forEach((product)=>{
               <option value="10">10</option>
             </select>
           </div>
-
+ 
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
@@ -79,8 +79,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
             quantity : 1
           });
         }
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
       
-      
+      console.log(cartQuantity);
       console.log(cart);
     });
 });
